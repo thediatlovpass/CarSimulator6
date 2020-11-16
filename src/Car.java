68ab6fe67b917706f1cefacfbc5ac2cf0f70ab56
wlodarczyk_p <thediatlovpass@gmail.com>
2020-11-16 22:10:21 +0100

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car {
+    private final static int CAR_IS_WORKING = 100;
+    private final static int CAR_IS_NOT_WORKING_ENGINE_FAILURE = 200;
+    private final static int CAR_IS_NOT_WORKING_LACK_OF_FUEL = 300;
+    private final static int CAR_IS_WORKING_FUEL_FILLER_FLAP_OPEN = 400;
+
+    private final static int
+}
